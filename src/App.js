4312b82a39db501232126de41172d7367f041ee5
kwoncha <rnjsckgus@gmail.com>
2023-11-23 12:54:04 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const { totalGame } = require('./playing/totalgame');
-
 class App {
-  async play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    await totalGame();
-  }
+  async play() { }
 }
 
 export default App;
