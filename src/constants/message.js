59706af0eef4bc5f2b261bbb4c603ae41d8e3026
kwoncha <rnjsckgus@gmail.com>
2023-11-23 15:21:15 +0900

    refactor: 출력에 맞게 일부 메세지 수정 및 불필요한 메세지 삭제

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,4 +1,4 @@
-import deepFreeze from '../utils/deepFreeze/deepFreeze';
+import deepFreeze from '../utils/deepFreeze/deepFreeze.js';
 
 const PREFIX = '[ERROR]';
 
@@ -7,9 +7,10 @@ const MESSAGE = deepFreeze({
   inputNumber: '숫자를 입력해주세요 : ',
   strike: strike => `${strike}스트라이크`,
   ball: ball => `${ball}볼`,
-  blink: ' ',
+  nothing: '낫싱',
+  threeStrike: '3스트라이크',
   endGame: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  restart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  restart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
   ERROR: {
     notValidNumber: `${PREFIX} 서로다른 3자리 숫자를 입력해주세요.`,
     notValidRestartNumber: `${PREFIX} 1, 2만 입력해주세요.`,
