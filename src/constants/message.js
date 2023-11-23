f599f7310f09a60887d49147dca3418ba8496e5a
kwoncha <rnjsckgus@gmail.com>
2023-11-23 13:28:41 +0900

    feat: 메세지 모듈 구현

---
+++ src/constants/message.js
@@ -0,0 +1,13 @@
+import deepFreeze from '../utils/deepFreeze/deepFreeze';
+
+const MESSAGE = deepFreeze({
+  startGame: '숫자 야구 게임을 시작합니다.',
+  inputNumber: '숫자를 입력해주세요 : ',
+  strike: '스트라이크',
+  ball: '볼',
+  blink: ' ',
+  endGame: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  restart: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+});
+
+export default MESSAGE;
